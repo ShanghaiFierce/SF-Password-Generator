823df48fa8ed3e2c71d99bb9162035e438f431a7
Shanghai Fierce <monitechi8@gmail.com>
2020-08-17 02:44:05 -0500

    tried to adj password

--- Develop/script.js
+++ Develop/script.js
@@ -9,93 +9,124 @@ function createPassword() {
 
     length = Number(length);
 
-    if (length < 8 || length > 124 || length === "") {
+    if (length < 8 || length > 124 || !length) {
         window.alert("Password length must be more than 8 characters and less than 124 characters long.");
-        return length();
+        createPassword ();
     }
     else {
         alert("You have chosen a password with a total of: " + length + " characters. Is this correct?")
-        return (length);
+        charCriteriaPrompt();
     };
-
-    charCriteriaPrompt();
 };
-
-// Run the SF Password Generator
-createPassword();
 
 //confirm which criteria the user wants in the password
 var charCriteriaPrompt = function () {
-    confirm("Please select at least 1 of the following in order to generate a safe password. Select to add a Lowercase Letter, an Uppercase Letter, a Number and/or a Special Character.");
+    alert("Please select at least 1 of the following in order to generate a safe password. A) Lowercase Letter B) Uppercase Letter C) a Number and/or D) a Special Character.");
+
+        // Prompt lowercase
+        var lcstring = prompt("Would you like to add a lowercase letter in your password? Please type Y for Yes and N for No.");
+        if (lcstring === "Y") {
+            confirm("We will add a lowercase letter to your password.");
+            console.log (lcstring);
+        }
+        else {
+            alert("You decided not to choose an lowercase letter, that's ok. Let's move on.");  
+        }
+        // Prompt uppercase
+        var upstring = prompt("Would you like to add a uppercase letter in your password? Please type Y for Yes and N for No.");
+        if (upstring === "Y") {
+            confirm("We will add a uppercase letter to your password.");
+            console.log (upstring);
+        }
+        else {
+            alert("You decided not to choose an uppercase letter, that's ok. Let's move on.");
+        }
+
+        // Prompt number
+        var num = prompt("Would you like to add a number in your password? Please type Y for Yes and N for No.");
+        if (num === "Y") {
+            confirm("We will add a number to your password.");
+            console.log (num);
+        }
+        else {
+            alert("You decided not to choose an number, that's ok. Let's move on.");
+        }
+
+        // Prompt special character
+        var spchar = prompt("Would you like to add a special character in your password? Please type Y for Yes and N for No.");
+        if (spchar === "Y") {
+            confirm("We will add a special character to your password.");
+            console.log (spchar);
+        }
+        else {
+            alert("You decided not to choose an special character, that's ok. Let's move on.");
+        }
+
+        //Final check for num, up, lc, spchar
+         if (lcstring === "Y" || upstring === "Y" || num === "Y" || spchar === "Y") {
+            confirm("We are getting ready to generate your unique password!");
+            generatePwd();
+         }
+         else {
+             //(!lcstring || lcstring === "N" && !upstring || upstring === "N" && !num || num === "N" && !spchar || spchar === "N") {
+            window.alert("You need to select at least 1 of the following to add to your password. Let's try again.");
+            return charCriteriaPrompt();
+        }
 }
 
-// Prompt lowercase
-var lcstring = prompt("Would you like to add a lowercase letter in your password? Please type Y for Yes and N for No.");
-if (lcstring === "Y") {
-    confirm("We will add a lowercase letter to your password.");
-    console.log(lcstring);
-}
-else {
-    alert("You decided not to choose an lowercase letter, that's ok. Let's move on.");
-}
+// Run the SF Password Rules
+createPassword();
 
-// Prompt uppercase
-var upstring = prompt("Would you like to add a uppercase letter in your password? Please type Y for Yes and N for No.");
-if (upstring === "Y") {
-    confirm("We will add a uppercase letter to your password.");
-    console.log(upstring);
-}
-else {
-    alert("You decided not to choose an uppercase letter, that's ok. Let's move on.");
-}
+// Generate function for the final password
+function generatePwd() {
+    var password = '';
+    var lcstring = "abcdefghijklmnopqrstuvwxyz";
+    var upstring = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var num = "0123456789";
+    var spchar = "!@#$%^&*()_+~`|}{[]\:;?>,./-";
 
-// Prompt number
-var num = prompt("Would you like to add a number in your password? Please type Y for Yes and N for No.");
-if (num === "Y") {
-    confirm("We will add a number to your password.");
-    console.log(num);
-}
-else {
-    alert("You decided not to choose an number, that's ok. Let's move on.");
-}
+    //Length "if & else" conditions
+    //lcstring result
+    var lcstringAfter = localStorage.getItem("lcstring");
+    if (lcstring === "Y") {
+        lcstring.length = 1;}
+    else {
+        lcstring.length = 0};
+    //upstring result
+    var upstringAfter = localStorage.getItem("upstring");
+        if (upstring === "Y") {
+        upstring.length = 1;}
+    else {
+        upstring.length = 0};
+    //num result
+    var numAfter = localStorage.getItem("num");
+        if (num === "Y") {
+        num.length = 1;}
+    else {
+        num.length = 0};
+    //spchar result
+    var spcharAfter = localStorage.getItem("spchar");
+        if (spchar === "Y") {
+        spchar.length = 1;}
+    else {
+        spchar.length = 0};
 
-// Prompt special character
-var spchar = prompt("Would you like to add a special character in your password? Please type Y for Yes and N for No.");
-if (spchar === "Y") {
-    confirm("We will add a special character to your password.");
-    console.log(spchar);
-}
-else {
-    alert("You decided not to choose an special character, that's ok. Let's move on.");
-}
+    //ceiling
+    var length = Math.ceil();
 
-generate();
-
-//Characters Critera
-//var charCriteria = {}
-var lcstring = "abcdefghijklmnopqrstuvwxyz";
-var upstring = "abcdefghijklmnopqrstuvwxyz";
-var num = "0123456789";
-var spchar = "!@#$%^&*()_+~`|}{[]\:;?><,./-";
-var userPassword = "";
-
-
-// // Generate the final password
-function generate() {
-    if (lcstring == "Y") {
-        userPassword += lcstring;
+    for ( i=0; i < length; i++) {
+        password += lcstring.charAt(Math.ceil(Math.random() * lcstring.length));
+        password += upstring.charAt(Math.ceil(Math.random() * upstring.length));
+        password += num.charAt(Math.ceil(Math.random() * num.length));
+        password += spchar.charAt(Math.ceil(Math.random() * spchar.length)).join('');
     }
-    if (upstring == "Y") {
-        userPassword += upstring;
-    }
-    if (num == "Y") {
-        userPassword += num;
-    }
-    if (spchar == "Y") {
-        userPassword += spchar;
-    }
-    return userPassword;
-}
+
+    return (password);
+// //Confirming the special variables
+// var charCriteria = [lcstring === "Y", upstring === "Y", num === "Y", spchar === "Y"];
+// var userPassword = charCriteria.join(" and ");
+// //return (userPassword);
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -108,4 +139,4 @@ function writePassword() {
 };
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generate);
+generateBtn.addEventListener("click", createPassword);
